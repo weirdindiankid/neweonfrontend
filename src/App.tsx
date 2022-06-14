5ac629dc9f3f6302615ba34ea2e4fdfb26a192d5
Christopher Burnette <chrisburnette083@gmail.com>
2022-06-13 22:08:54 -0600

    Added basic dark/light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./GlobalStyles";
+import { lightTheme, darkTheme } from "./Themes"
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
   return (
-    <>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <button onClick={themeToggler}>Switch Theme</button>
       <h1>Eon</h1>
-    </>
+    </ThemeProvider>
   );
 }
 
