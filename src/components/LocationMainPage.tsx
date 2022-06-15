d6ace79a56880e2cb8b9f189b01034bb137ccd28
Christopher Burnette <chrisburnette083@gmail.com>
2022-06-15 16:07:43 +0500

    Added new data for car list

--- src/components/LocationMainPage.tsx
+++ src/components/LocationMainPage.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { SuggestedLocation } from "./SuggestedLocation";
 import carImg from "../assets/car_img.png";
+import blackCar from "../assets/black-car.png"
+import grayCar from "../assets/gray_car.png"
+import whiteCar from "../assets/white_car.png"
 import cartoonIcon from "../assets/smart_toy.svg";
 import spaeedIcon from "../assets/speed.svg";
 import mileIcon from "../assets/battery_4_bar.svg";
@@ -11,6 +14,122 @@ import { CarList } from "./CarList";
 
 const data1 = [{}];
 const data2 = [
+  {
+    img: carImg,
+    model: "Model 3",
+    disc: "Performance",
+    numDays: "169 / day",
+    numHours: "338 / total",
+    features: [
+      {
+        icon: cartoonIcon,
+        title: "Enhanced autopilot",
+      },
+      {
+        icon: spaeedIcon,
+        title: "0-60 in 4.4s",
+      },
+      {
+        icon: mileIcon,
+        title: "310 miles",
+      },
+      {
+        icon: seatIcon,
+        title: "5 seats",
+      },
+      {
+        icon: intrImg,
+        title: "Interior",
+      },
+    ],
+  },
+  {
+    img: blackCar,
+    model: "Model Y",
+    disc: "Long range",
+    numDays: "199 / day",
+    numHours: "398 / total",
+    features: [
+      {
+        icon: cartoonIcon,
+        title: "Full self driving",
+      },
+      {
+        icon: spaeedIcon,
+        title: "0-60 in 4.4s",
+      },
+      {
+        icon: mileIcon,
+        title: "326 miles",
+      },
+      {
+        icon: seatIcon,
+        title: "5 seats",
+      },
+      {
+        icon: intrImg,
+        title: "Interior",
+      },
+    ],
+  },
+  {
+    img: grayCar,
+    model: "Model X",
+    disc: "P100D",
+    numDays: "219 / day",
+    numHours: "438 / total",
+    features: [
+      {
+        icon: cartoonIcon,
+        title: "Enhanced autopilot",
+      },
+      {
+        icon: spaeedIcon,
+        title: "0-60 in 4.9s",
+      },
+      {
+        icon: mileIcon,
+        title: "295 miles",
+      },
+      {
+        icon: seatIcon,
+        title: "5 seats",
+      },
+      {
+        icon: intrImg,
+        title: "Interior",
+      },
+    ],
+  },
+  {
+    img: whiteCar,
+    model: "Model S",
+    disc: "Palladium",
+    numDays: "299 / day",
+    numHours: "598 / total",
+    features: [
+      {
+        icon: cartoonIcon,
+        title: "Standard autopilot",
+      },
+      {
+        icon: spaeedIcon,
+        title: "0-60 in 3.1s",
+      },
+      {
+        icon: mileIcon,
+        title: "405 miles",
+      },
+      {
+        icon: seatIcon,
+        title: "5 seats",
+      },
+      {
+        icon: intrImg,
+        title: "Interior",
+      },
+    ],
+  },
   {
     img: carImg,
     model: "Model 3",
