b2d08c5169531d0173f2314ffd8fe8a993f9fcf5
Christopher Burnette <chrisburnette083@gmail.com>
2022-06-25 05:51:11 -0500

    implemented expand panels in car detail view

--- src/components/DetailView.tsx
+++ src/components/DetailView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import arrowLeftLight from "../assets/arrow_back_light.svg";
 import arrowLeftDark from "../assets/arrow_back_dark.svg";
 import Form from "react-bootstrap/Form";
@@ -14,6 +14,14 @@ export const DetailView: React.FC<Types> = ({
   onChangeSection,
   data,
 }: Types) => {
+  const [checkBox, setCheckBox] = useState<boolean>(false);
+  const [checkBox2, setCheckBox2] = useState<boolean>(false);
+  const [anything, setAnything] = React.useState({
+    unlimited: false,
+    roadside: false,
+    discount: false,
+  });
+
   const mystyle = {
     backgroundColor: !darkMode ? "#F5F5F5" : "#1F1F1F",
     borderRadius: "20px",
@@ -120,6 +128,29 @@ export const DetailView: React.FC<Types> = ({
           <div>
             <span className="fs-4 fw-bold">Choose insurance</span>
           </div>
+          {/* <div className="w-100">
+            <div
+              className="py-2 px-3 my-3 shadow-sm rounded d-flex justify-content-between"
+              style={nestedCards}
+            >
+              <div className="me-2">
+                <Form>
+                  <Form.Check
+                    className="fs-6 fw-bold"
+                    inline
+                    label="Your insurance"
+                    name="group1"
+                    type="radio"
+                  />
+                </Form>
+              </div>
+              <div>
+                <span className="fs-6 fw-bold" style={{ color: "GrayText" }}>
+                  Free
+                </span>
+              </div>
+            </div>
+          </div> */}
           <div className="mt-3">
             <div className="border border-secondary rounded p-3 d-flex justify-content-between">
               <div className="d-flex flex-column lh-sm">
@@ -130,17 +161,24 @@ export const DetailView: React.FC<Types> = ({
                     label="Your insurance"
                     name="group1"
                     type="radio"
+                    checked={checkBox}
+                    onClick={() => {
+                      setCheckBox(!checkBox);
+                      setCheckBox2(false);
+                    }}
                   />
                 </Form>
-                <small className="px-4" style={{ fontSize: "12px" }}>
-                  Before you pick up the car, we’ll ask you to upload a copy of
-                  your insurance agreement.
-                </small>
+                {checkBox ? (
+                  <small className="px-4" style={{ fontSize: "12px" }}>
+                    Before you pick up the car, we’ll ask you to upload a copy
+                    of your insurance agreement.
+                  </small>
+                ) : null}
               </div>
               <span className="fs-6 fw-bold">Free</span>
             </div>
           </div>
-          <div className="w-100">
+          {/* <div className="w-100">
             <div
               className="py-2 px-3 my-3 shadow-sm rounded d-flex justify-content-between"
               style={nestedCards}
@@ -162,6 +200,33 @@ export const DetailView: React.FC<Types> = ({
                 </span>
               </div>
             </div>
+          </div> */}
+          <div className="mt-3">
+            <div className="border border-secondary rounded p-3 d-flex justify-content-between">
+              <div className="d-flex flex-column lh-sm">
+                <Form>
+                  <Form.Check
+                    className="fs-6 fw-bold"
+                    inline
+                    label="Our insurance"
+                    name="group2"
+                    type="radio"
+                    checked={checkBox2}
+                    onClick={() => {
+                      setCheckBox2(!checkBox2);
+                      setCheckBox(false);
+                    }}
+                  />
+                </Form>
+                {checkBox2 ? (
+                  <small className="px-4" style={{ fontSize: "12px" }}>
+                    Covers damage up to the full vehicle value with a $500
+                    deductible. Liability coverage up to the state minimum.
+                  </small>
+                ) : null}
+              </div>
+              <span className="fs-6 fw-bold text-nowrap">+76.65 / day</span>
+            </div>
           </div>
         </div>
 
@@ -169,8 +234,7 @@ export const DetailView: React.FC<Types> = ({
           <div>
             <span className="fs-4 fw-bold">Anything else?</span>
           </div>
-
-          <div className="w-100">
+          {/* <div className="w-100">
             <div
               className="py-2 px-3 my-3 shadow-sm rounded d-flex justify-content-between"
               style={nestedCards}
@@ -191,8 +255,35 @@ export const DetailView: React.FC<Types> = ({
                 </span>
               </div>
             </div>
+          </div> */}
+          <div className="mt-3">
+            <div className="border border-secondary rounded p-3 d-flex justify-content-between">
+              <div className="d-flex flex-column lh-sm">
+                <Form>
+                  <Form.Check
+                    className="fs-6 fw-bold"
+                    inline
+                    label="Unlimited toll usage"
+                    name="group1"
+                    checked={anything.unlimited}
+                    onClick={() => {
+                      setAnything({
+                        ...anything,
+                        unlimited: !anything.unlimited,
+                      });
+                    }}
+                  />
+                </Form>
+                {anything.unlimited ? (
+                  <small className="px-4" style={{ fontSize: "12px" }}>
+                    Don’t worry about toll road payments.
+                  </small>
+                ) : null}
+              </div>
+              <span className="fs-6 fw-bold text-nowrap">+9 / day</span>
+            </div>
           </div>
-          <div className="w-100">
+          {/* <div className="w-100">
             <div
               className="py-2 px-3 my-3 shadow-sm rounded d-flex justify-content-between"
               style={nestedCards}
@@ -213,8 +304,36 @@ export const DetailView: React.FC<Types> = ({
                 </span>
               </div>
             </div>
+          </div> */}
+          <div className="mt-3">
+            <div className="border border-secondary rounded p-3 d-flex justify-content-between">
+              <div className="d-flex flex-column lh-sm">
+                <Form>
+                  <Form.Check
+                    className="fs-6 fw-bold"
+                    inline
+                    label="Roadside protection"
+                    name="group1"
+                    checked={anything.roadside}
+                    onClick={() => {
+                      setAnything({
+                        ...anything,
+                        roadside: !anything.roadside,
+                      });
+                    }}
+                  />
+                </Form>
+                {anything.roadside ? (
+                  <small className="px-4" style={{ fontSize: "12px" }}>
+                    Covers you for tire or windshield replacement and any
+                    related roadside assistance.
+                  </small>
+                ) : null}
+              </div>
+              <span className="fs-6 fw-bold text-nowrap">+13 / day</span>
+            </div>
           </div>
-          <div className="w-100">
+          {/* <div className="w-100">
             <div
               className="py-2 px-3 my-3 shadow-sm rounded"
               style={nestedCards}
@@ -230,7 +349,68 @@ export const DetailView: React.FC<Types> = ({
                 </Form>
               </div>
             </div>
+          </div> */}
+          <div className="mt-3">
+            <div className="border border-secondary rounded p-3 d-flex justify-content-between">
+              <div className="d-flex flex-column w-75">
+                <Form>
+                  <Form.Check
+                    className="fs-6 fw-bold"
+                    inline
+                    label="Add discount code"
+                    name="group1"
+                    checked={anything.discount}
+                    onClick={() => {
+                      setAnything({
+                        ...anything,
+                        discount: !anything.discount,
+                      });
+                    }}
+                  />
+                </Form>
+                {anything.discount ? (
+                  <>
+                    <div>
+                      <Form.Floating>
+                        <Form.Control
+                          style={mystyle}
+                          id="floatingEnterCode"
+                          type="text"
+                          placeholder="↑↑↓↓←→←→BA"
+                          className={
+                            !darkMode
+                              ? "border-0 text-dark rounded"
+                              : "border-0 rounded text-white"
+                          }
+                        />
+                        <label htmlFor="floatingEnterCode">Enter code</label>
+                      </Form.Floating>
+                    </div>
+                    <div className="my-3">
+                      <button type="button" className="btn btn-outline-dark">
+                        Redeem code
+                      </button>
+                    </div>
+                    <div>
+                      <span>Your code is worth 25.-</span>
+                    </div>
+                  </>
+                ) : null}
+              </div>
+              <span className="fs-6 fw-bold text-danger text-nowrap">-25</span>
+            </div>
           </div>
+        </div>
+      </div>
+      <hr />
+      <div className="m-2 px-2">
+        <div className="d-flex justify-content-between align-items-center">
+          <span className="fs-6 fw-bold">Total for 2 days</span>
+          <span className="fs-6 fw-bold">720.30</span>
+        </div>
+        <div className="d-flex justify-content-between align-items-center">
+          <span className="text-danger">Total for 2 days</span>
+          <span className="text-danger">720.30</span>
         </div>
       </div>
 
