6647ea9352cf0c03beae7f97e2e94187f5f1bd9b
Christopher Burnette <chrisburnette083@gmail.com>
2022-07-05 07:40:54 +0500

    Added API base URL from env

--- src/api/API.ts
+++ src/api/API.ts
@@ -4,7 +4,7 @@
  * Description: API wrapper for the Eon Api.
  */
 
-let API_BASE_URL = process.env.API_URL || 'http://localhost:3000/api/v1';
+let API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api/v1';
 
 export const retrieveCities = async () => {
     const response = await fetch(`${API_BASE_URL}/rental_locations/cities`);
